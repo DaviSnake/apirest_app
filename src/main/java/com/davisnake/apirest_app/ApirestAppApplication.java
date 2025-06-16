623f21a8ce54733058dd84fbd6b9c449f2b06f42
David Medina <david.medina@starken.cl>
2025-06-15 20:36:48 -0400

    primer commit

---
+++ src/main/java/com/davisnake/apirest_app/ApirestAppApplication.java
@@ -0,0 +1,13 @@
+package com.davisnake.apirest_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davisnake/apirest_app/ApirestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davisnake.apirest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
